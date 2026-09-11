9bcdf53232313558101eb0ed6b4435dc6f03884b
Nexo Informática <nexoinfo801@gmail.com>
2026-09-11 20:07:33 -0300

    mobile: refresh cache for embedded NEXA avatar

--- gateway/public/mobile/sw.js
+++ gateway/public/mobile/sw.js
@@ -1,4 +1,4 @@
-const CACHE='nexa-mobile-r12-v1';
+const CACHE='nexa-mobile-r12-v2';
 const ASSETS=['./','./index.html','./app.css','./mobile-v2.css','./mobile-v2.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
