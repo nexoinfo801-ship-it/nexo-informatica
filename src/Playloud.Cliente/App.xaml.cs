5dec5354c1f6564a516ed5746effa9e6090098d5
Nexo Informática <nexoinfo801@gmail.com>
2026-09-16 21:46:33 -0300

    fix(cliente): qualify WPF application base type

--- src/Playloud.Cliente/App.xaml.cs
+++ src/Playloud.Cliente/App.xaml.cs
@@ -3,7 +3,7 @@ using Playloud.Cliente.Bootstrap;
 
 namespace Playloud.Cliente;
 
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
     private ClienteApplicationHost? _host;
 
