9c1e7c4a49495992f23e0b6f20bb40d73d25801c
Nexo Informática <nexoinfo801@gmail.com>
2026-09-16 21:28:54 -0300

    feat(cliente): inject checkout view model into MainWindow

--- src/Playloud.Cliente/Shell/MainWindow.xaml.cs
+++ src/Playloud.Cliente/Shell/MainWindow.xaml.cs
@@ -1,11 +1,15 @@
 using System.Windows;
+using Playloud.Cliente.Sales;
 
 namespace Playloud.Cliente.Shell;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
+    public MainWindow(SaleCheckoutViewModel viewModel)
     {
+        ArgumentNullException.ThrowIfNull(viewModel);
+
         InitializeComponent();
+        DataContext = viewModel;
     }
 }
